b54739d109f187cb51e273f28deb193c3ac9edd9
busybox <remi.chartier@viacesi.fr>
2022-12-15 16:53:27 +0100

    project: Basic API route and temp show

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
-import sqlite3
-sqlcon = sqlite3.connect("database/sensors.db")
+import requests
 
+import sqlite3
+
+sqlcon = sqlite3.connect("database/sensors.db")
 cur = sqlcon.cursor()
 cur.executescript("""
 CREATE TABLE IF NOT EXISTS receivers (id INTEGER PRIMARY KEY, imei INT, marque_receiver TEXT);
@@ -38,32 +40,35 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+class RemoteSensor(Resource):
+    def get(self):
+        response = requests.get("http://app.objco.com:8099/?account=GX1GLQRVNM&limit=6")
+        dico = str(response.json())
+        capteurs = ["06182660", "62182233", "06190484"]
+        capteursObj = {}
+        for element in capteurs:
+            position = dico.find(element)
+            temp = str((dico[position + 14:position + 18]))
+            rssi = str((dico[position + 20:position + 22]))
+            convertion_temp = int(temp, 16) / 10
+            convertion_rssi = int(rssi, 16)
+            isanormal = (convertion_temp - 32768) > 0
+            isneg = (convertion_temp - 16384) > 0
+            capteursObj[element] = {
+                "temp": convertion_temp,
+                "rssi": convertion_rssi,
+                "isanormal": isanormal,
+                "isneg": isneg
+            }
+            if element == "62182233":
+                humid = str((dico[position + 18:position + 20]))
+                convertion_humid = int(humid, 16)
+                capteursObj[element]["humid"] = convertion_humid
+
+        return capteursObj
+
 api.add_resource(HelloWorld, '/api/hello')
+api.add_resource(RemoteSensor, '/api/remotesensor')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-##########################################################################################################
-import requests
-response = requests.get("http://app.objco.com:8099/?account=GX1GLQRVNM&limit=6")
-dico = str(response.json())
-capteurs = ["06182660", "62182233", "06190484"]
-for element in capteurs:
-    position = dico.find(element)
-    temp = str((dico[position + 14:position + 18]))
-    rssi = str((dico[position + 20:position + 22]))
-    convertion_temp = int(temp, 16)
-    convertion_rssi = int(rssi, 16)
-    isanormal = (convertion_temp - 32768) > 0
-    isneg = (convertion_temp - 16384) > 0
-    if not isanormal:
-        if isneg:
-            print("Température du capteur " + element, ": -" + str((convertion_temp - 16384)/10), "°C")
-        else :
-            print("Température du capteur " + element, ":", str(convertion_temp/10), "°C")
-    else : print("Température anormale")
-    if element == "62182233" :
-        humid = str((dico[position + 18:position + 20]))
-        convertion_humid = int(humid, 16)
-        print("Humidité du capteur " + element, ":", str(convertion_humid) + "%")
-    print("RSSI du capteur " + element, ": -" + str(convertion_rssi), "dBm")
