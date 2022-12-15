f7cc756d1f6b8bedaa8aaf9332df218092ebf162
busybox <remi.chartier@viacesi.fr>
2022-12-15 12:14:00 +0100

    main: Serve website as static files

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 api = Api(app)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/api/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
