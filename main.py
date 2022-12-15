d7213af6107e0223fd45eb6c2dc64a84df50e99f
Alexis <alexis.loison@outlook.fr>
2022-12-15 16:37:40 +0100

    changed python file to have sneors info + MCD/MLD

--- main.py
+++ main.py
@@ -42,3 +42,28 @@ api.add_resource(HelloWorld, '/api/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+##########################################################################################################
+import requests
+response = requests.get("http://app.objco.com:8099/?account=GX1GLQRVNM&limit=6")
+dico = str(response.json())
+capteurs = ["06182660", "62182233", "06190484"]
+for element in capteurs:
+    position = dico.find(element)
+    temp = str((dico[position + 14:position + 18]))
+    rssi = str((dico[position + 20:position + 22]))
+    convertion_temp = int(temp, 16)
+    convertion_rssi = int(rssi, 16)
+    isanormal = (convertion_temp - 32768) > 0
+    isneg = (convertion_temp - 16384) > 0
+    if not isanormal:
+        if isneg:
+            print("Température du capteur " + element, ": -" + str((convertion_temp - 16384)/10), "°C")
+        else :
+            print("Température du capteur " + element, ":", str(convertion_temp/10), "°C")
+    else : print("Température anormale")
+    if element == "62182233" :
+        humid = str((dico[position + 18:position + 20]))
+        convertion_humid = int(humid, 16)
+        print("Humidité du capteur " + element, ":", str(convertion_humid) + "%")
+    print("RSSI du capteur " + element, ": -" + str(convertion_rssi), "dBm")
