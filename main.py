950a690f8e217b2726f049821840b1318835abdc
busybox <remi.chartier@viacesi.fr>
2023-01-02 23:56:48 +0100

    AYOTI-8 - Listen flask server on all adresses

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ api.add_resource(HelloWorld, '/api/hello')
 api.add_resource(RemoteSensor, '/api/remotesensor')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
